dcb90606397039b6467dd467feb4a3a4f0280468
mkirc <>
2021-07-21 23:44:40 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+This is a handy little utility for producing
+mathjax-supported html from markdown.
+My usecase is writing summaries of lectures.
+
+Usage: bash makeOverview.bash [your md file]
+
+enjoy!
---
+++ makeOverview.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pandoc --from markdown_github+smart+yaml_metadata_block+auto_identifiers+tex_math_single_backslash+tex_math_dollars "$1" \
+    --to=html5 \
+    -o overview.html \
+    --template='./assets/templates/page.html' \
+    --self-contained \
+    --mathjax\
+    --toc
