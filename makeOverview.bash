926ca629c08369bcc942ee78b47ab4adefd98d85
mkirc <mkirc@devs>
2021-10-15 11:16:14 +0200

    adds rudimental called-from-anywhere usability

--- makeOverview.bash
+++ makeOverview.bash
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+basePath="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 pandoc --from markdown_github+smart+yaml_metadata_block+auto_identifiers+tex_math_single_backslash+tex_math_dollars "$1" \
     --to=html5 \
-    -o overview.html \
-    --template='./assets/templates/page.html' \
+    -o "$(pwd)"/"${1/%.*/.html}" \
+    --template="$basePath"/assets/templates/page.html \
+    -V basepath="$basePath" \
     --self-contained \
     --mathjax\
     --metadata date="$(date)" \
     --metadata author='mkirc' \
-    --metadata title='tp2 overview' \
+    --metadata title=${1%%.*} \
     --toc
