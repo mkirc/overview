8fe9273f66cfa6a685f8a3d7e53630fb9ce4eb49
mkirc <m.p.kirchner@gmx.de>
2022-03-31 17:39:27 +0200

    adds command line options, updates README

--- README.txt
+++ README.txt
@@ -1,19 +1,17 @@
 This is a handy little utility for producing
 mathjax-supported html from markdown.
-My usecase is writing summaries of lectures.
+I use it for writing summaries of lectures.
+
+Installation: clone this repo.
+You can also add it into your $PATH or just make an
+alias overview="bash path/to/makeOverview.bash".
 
 Usage:
 
-You can use this fully featured (eg. with embedded images etc.) by
-cloning the repo, copying the .md you want to convert into the project
-and compile via
+bash /path/to/makeOverview.bash <your md file> [-o,--outfile your-file.html]
 
-bash makeOverview.bash [your md file]
+Hat tip to https://argbash.io. This project uses argbash-utilities
+to generate its command line options and help message.
 
-You can also add it into your $PATH or just make an
-
-alias overview="bash path/to/the/repo/overview/makeOverview.bash"
-
-and use this for quick compilation (right now only text works).
 
 enjoy!
--- makeOverview.bash
+++ makeOverview.bash
@@ -2,14 +2,38 @@
 
 basePath="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-pandoc --from markdown_github+smart+yaml_metadata_block+auto_identifiers+tex_math_single_backslash+tex_math_dollars "$1" \
-    --to=html5 \
-    -o "$(pwd)"/"${1/%.*/.html}" \
-    --template="$basePath"/assets/templates/page.html \
-    -V basepath="$basePath" \
-    --self-contained \
-    --mathjax\
-    --metadata date="$(date)" \
-    --metadata author='mkirc' \
-    --metadata title=${1%%.*} \
-    --toc
+# source command line options
+. "$basePath"/ov-opts.sh
+
+set_default_arguments() {
+
+    if [[ -z "$_arg_md_file_path" ]]; then
+        _PRINT_HELP=yes die
+    elif [[ -z "$_arg_outfile" ]]; then
+        _arg_outfile="$(pwd)"/"${_arg_md_file_path/%.*/.html}"
+    fi
+
+}
+
+run_pandoc(){
+
+    pandoc --from markdown_github+smart+yaml_metadata_block+auto_identifiers+tex_math_single_backslash+tex_math_dollars "$_arg_md_file_path" \
+        --to=html5 \
+        -o "$_arg_outfile" \
+        --template="$basePath"/assets/templates/page.html \
+        -V basepath="$basePath" \
+        --self-contained \
+        --mathjax\
+        --metadata date="$(date)" \
+        --metadata author="$USER" \
+        --metadata title=${_arg_md_file_path%%.*} \
+        --toc
+
+}
+
+main() {
+    set_default_arguments
+    run_pandoc
+}
+
+main
