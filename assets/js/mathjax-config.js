4f0900ec554a6918d7157aef51addfb65f300f3e
mkirc <m.p.kirchner@gmx.de>
2022-10-22 01:10:52 +0200

    removes faulty mathjax config

--- assets/js/mathjax-config.js
+++
@@ -1,8 +0,0 @@
-
-MathJax.Hub.StartupHook("MathMenu Ready", function () {
-    MathJax.Menu.menu.Find("About MathJax").hidden = true;
-    MathJax.Menu.menu.Find("Show Math As").hidden = true;
-    MathJax.Menu.menu.Find("MathJax Help").hidden = true;
-});
-
-
