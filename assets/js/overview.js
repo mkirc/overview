4634dc195f51c58ff069f40490b8aa75355dc894
mkirc <m.p.kirchner@gmx.de>
2023-02-17 16:43:24 +0100

    fixes indentation, sort definitions alphabetically

--- assets/js/overview.js
+++ assets/js/overview.js
@@ -2,114 +2,123 @@
 setInitialColorScheme();
 
 window.addEventListener("load", function(e) {
-    attachTocMouseEventListeners();
+  attachTocMouseEventListeners();
 
 })
 
 function setInitialColorScheme() {
 
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-    if ( localStorage.getItem('color-mode') === 'light' ) {
-        document.documentElement.setAttribute('color-mode', 'light')
-    } else if ( prefersDark || localStorage.getItem('color-mode') === 'dark' ) {
-        document.documentElement.setAttribute('color-mode', 'dark')
-    }
+  if ( localStorage.getItem('color-mode') === 'light' ) {
+    document.documentElement.setAttribute('color-mode', 'light')
+  } else if ( prefersDark || localStorage.getItem('color-mode') === 'dark' ) {
+    document.documentElement.setAttribute('color-mode', 'dark')
+  }
 }
 
 
 function attachTocMouseEventListeners() {
 
-    const tocUls = document.querySelectorAll('#toc-details ul');
+  const tocUls = document.querySelectorAll('#toc-details ul');
 
-    tocUls.forEach((elm, idx) => {
-        if (idx > 0) {
-            elm.addEventListener('mouseover', function(e) {
-                elm.previousElementSibling.classList.add("highl-txt");
-            });
-            elm.addEventListener('mouseout', function(e) {
-                elm.previousElementSibling.classList.remove("highl-txt")
-            });
-        }
-    });
+  tocUls.forEach((elm, idx) => {
+    if (idx > 0) {
+      elm.addEventListener('mouseover', function(e) {
+        elm.previousElementSibling.classList.add("highl-txt");
+      });
+      elm.addEventListener('mouseout', function(e) {
+        elm.previousElementSibling.classList.remove("highl-txt")
+      });
+    }
+  });
 }
 
 function toggleTOC() {
 
-    const grid = document.getElementById('grid-container');
+  const grid = document.getElementById('grid-container');
 
-    if (grid.getAttribute('toc-visible') == 'init') {
-        grid.setAttribute('toc-visible', 'toggle')
-    } else if (grid.getAttribute('toc-visible') == 'toggle') {
-        grid.setAttribute('toc-visible', 'init')
-    }
+  if (grid.getAttribute('toc-visible') == 'init') {
+    grid.setAttribute('toc-visible', 'toggle')
+  } else if (grid.getAttribute('toc-visible') == 'toggle') {
+    grid.setAttribute('toc-visible', 'init')
+  }
 
 
 }
 
 function toggleDefinitions() {
 
-    const grid = document.getElementById('grid-container');
-    const definitionsEntries = document.querySelectorAll("#definitions > ul li");
+  const grid = document.getElementById('grid-container');
+  const definitionsEntries = document.querySelectorAll("#definitions > ul li");
 
-    if (grid.getAttribute('defs-visible') == 'init') {
-        grid.setAttribute('defs-visible', 'toggle')
-        if (definitionsEntries.length === 0) {
-            const definitions = crawlDefinitions();
-            makeDefinitionList(definitions);
-        }
-    } else if (grid.getAttribute('defs-visible') == 'toggle') {
-        grid.setAttribute('defs-visible', 'init')
+  if (grid.getAttribute('defs-visible') == 'init') {
+    grid.setAttribute('defs-visible', 'toggle')
+    if (definitionsEntries.length === 0) {
+      const definitions = crawlDefinitions();
+      makeDefinitionUL(sortNodesByTextContent(definitions));
     }
+  } else if (grid.getAttribute('defs-visible') == 'toggle') {
+    grid.setAttribute('defs-visible', 'init')
+  }
 }
 
 function crawlDefinitions() {
 
-    const definitions = document.querySelectorAll(".definition strong");
+  const definitions = document.querySelectorAll(".definition strong");
 
-    return definitions
+  return definitions
 }
 
-function makeDefinitionList(definitions) {
-    
-    const definitionsUl = document.querySelector("#definitions > ul");
+function sortNodesByTextContent(nodes) {
 
-    definitions.forEach((elm, idx) => {
+  let sorted = Array.from(nodes).sort(function(a,b) {
+    return a.textContent.localeCompare(b.textContent)
+  });
 
-        let entryLI = document.createElement("li");
+  return sorted
+}
 
-        entryLI.textContent = elm.textContent
+function makeDefinitionUL(definitions) {
 
-        entryLI.addEventListener("click", function(e) {
-            elm.scrollIntoView({behavior: "smooth", block: "center"})
-        })
+  const definitionsUl = document.querySelector("#definitions > ul");
 
-        definitionsUl.appendChild(entryLI);
+  definitions.forEach((elm, idx) => {
 
+    let entryLI = document.createElement("li");
+
+    entryLI.textContent = elm.textContent
+
+    entryLI.addEventListener("click", function(e) {
+      elm.scrollIntoView({behavior: "smooth", block: "center"})
     })
+
+    definitionsUl.appendChild(entryLI);
+
+  })
 }
 
 function toggleColorScheme() {
 
-    if ( document.documentElement.getAttribute('color-mode') == 'dark' ) {
-        document.documentElement.setAttribute('color-mode', 'light')
-        localStorage.setItem("color-mode", "light")
-    } else if (  document.documentElement.getAttribute('color-mode') == 'light' ) {
-        document.documentElement.setAttribute('color-mode', 'dark')
-        localStorage.setItem("color-mode", "dark")
-    }
-    repaintAllTocULs();
+  if ( document.documentElement.getAttribute('color-mode') == 'dark' ) {
+    document.documentElement.setAttribute('color-mode', 'light')
+    localStorage.setItem("color-mode", "light")
+  } else if (  document.documentElement.getAttribute('color-mode') == 'light' ) {
+    document.documentElement.setAttribute('color-mode', 'dark')
+    localStorage.setItem("color-mode", "dark")
+  }
+  repaintAllTocULs();
 
 }
 
 function repaintAllTocULs() {
 
-    const tocLinks = document.querySelectorAll("#toc-details a");
-    const root = document.querySelector(':root');
-    let rs = getComputedStyle(root);
+  const tocLinks = document.querySelectorAll("#toc-details a");
+  const root = document.querySelector(':root');
+  let rs = getComputedStyle(root);
 
-    tocLinks.forEach((elm, idx) => {
-        elm.style.color = rs.getPropertyValue('--main-txt-color');
-    });
+  tocLinks.forEach((elm, idx) => {
+    elm.style.color = rs.getPropertyValue('--main-txt-color');
+  });
 }
 
