526b2be640c592eee6ec4acf0d7a431ffe236336
mkirc <m.p.kirchner@gmx.de>
2025-02-14 19:09:09 +0100

    fixes jumping to ids with a period

--- assets/js/temml-loader.js
+++ assets/js/temml-loader.js
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // to keep focus of the current position on reload
     // we save the uri fragment and scroll to it after mml conversion
-    const current_hash = decodeURIComponent(window.location.hash);
+    const current_hash = decodeURIComponent(window.location.hash).substring(1);
 
     simpleLoader();
-    
+
     if (current_hash) {
-        const curr_h_elm = document.querySelector(current_hash);
+        const curr_h_elm = document.getElementById(current_hash);
         curr_h_elm.scrollIntoView();
-    } 
+    }
 });
