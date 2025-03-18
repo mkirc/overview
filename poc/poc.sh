6969af8db1baa2a0bc1c71a8fd571d34b2d26a81
mkirc <m.p.kirchner@gmx.de>
2025-03-18 22:26:00 +0100

    comments on poc

--- poc/poc.sh
+++ poc/poc.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
-# generate template from templates
-pandoc body.html \
+
+# step 0:
+# parse well-known filenames from appropriate (specified?)
+# module directory in form:
+# module-name/
+#            /html/
+#            /css/
+#            /js/
+
+# step 1:
+# assemble templates for left, right panels
+# as (-B, --include-before / -A, --include-after ).
+# body.html only contains '${ body }' and replaces
+# the '${ body }' in main template.
+
+# body.html effectively replaces '${ body }' in main.hmtml
+# --embed-resources effectively inlines css+js assets
+# -c add css assets by path
+# -H adds js assets by path
+# -B adds html asset in left panel
+# -A adds html asset in right panel
+# -o write template to temp file
+pandoc \
+    body.html \
     -s \
     --quiet \
     --template=main.html \
@@ -13,10 +35,19 @@ pandoc body.html \
     -A toc.html \
     -o tmp.html
 
-pandoc ./in.md -s \
+# step 1.1:
+# test exit status etc.
+
+# step 2:
+# use the template created in step 1 on the
+# infile. add toc if the module usage commands it.
+pandoc ./in.md \
+    -s \
     --quiet \
     --template=tmp.html \
     --toc \
     -o out.html \
 
+# step 666:
+# cleanup temp file
 [[ -f tmp.html ]] && cat tmp.html  && rm tmp.html
